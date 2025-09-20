7d0a4eb6550bec3823738c8355a537a7956fab1c
Ashutosh Rudraksh <rudrakshashutosh@gmail.com>
2025-09-20 19:24:04 -0400

    first commit with loading basic front-end javaScript & backend node.js file

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log("Script js is working");
+console.error("error")
+console.warn("warn")
+
+// single line comment
+
+/*
+multiple line comment
+*/
+
+// variables
+let name = "John";
+const age = 20;
+const isStudent = true;
+// using variables in console
+console.log(`${name} is ${age} years old and is a student: ${isStudent}`)
+
+// user input backend side
