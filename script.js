07a93690b8367b2ab3627a2888606340a2e7bea2
Ashutosh Rudraksh <rudrakshashutosh@gmail.com>
2025-09-20 21:14:35 -0400

    Covered before conditional loops

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("Script js is working");
+console.log("Script js is working")
 console.error("error")
 console.warn("warn")
 
@@ -9,10 +9,15 @@ multiple line comment
 */
 
 // variables
-let name = "John";
-const age = 20;
-const isStudent = true;
+let name = "John"
+const age = 20
+const isStudent = true
 // using variables in console
 console.log(`${name} is ${age} years old and is a student: ${isStudent}`)
 
-// user input backend side
+// user input backend side using npm(node package manager) using prompt function
+// $ npm init -y (to create a package.json file)
+// $ npm install prompt-sync (to install the prompt-sync package to use prompt function)
+const prompt = require("prompt-sync")() // to use prompt function
+const v = prompt("Enter your name: ")
+console.log(v)
